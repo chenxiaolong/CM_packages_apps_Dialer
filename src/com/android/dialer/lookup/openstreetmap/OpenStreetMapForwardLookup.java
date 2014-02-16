d906f6ca2e8b0f95f656225d6d3e955010fc0f0e
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2014-02-15 21:04:15 -0500

    My old email is gone
    Change-Id: Icfeccd195b9fbd269dc3400194dd42f215859049

--- src/com/android/dialer/lookup/openstreetmap/OpenStreetMapForwardLookup.java
+++ src/com/android/dialer/lookup/openstreetmap/OpenStreetMapForwardLookup.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2014 The OmniROM Project
- * Copyright (C) 2014 Xiao-Long Chen <chenxiaolong@cxl.epac.to>
+ * Copyright (C) 2014 Xiao-Long Chen <chillermillerlong@hotmail.com>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
